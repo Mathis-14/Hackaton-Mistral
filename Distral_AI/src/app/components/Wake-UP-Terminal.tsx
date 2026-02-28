2edf953b7a83dfc9c5bb58991f24f74cbd1ef878
eliottvalette <eliottvalette@gmail.com>
2026-02-28 15:25:36 +0100

    feat : click wake up

---
+++ Distral_AI/src/app/components/Wake-UP-Terminal.tsx
@@ -0,0 +1,14 @@
+export default function WakeUpTerminal() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black px-6 text-center">
+      <div className="space-y-4 text-[#70e000] [font-family:'VCR OSD Mono',Arial,sans-serif]">
+        <p className="text-[0.9rem] uppercase tracking-[0.45em] text-[#70e000]/55">
+          Terminal
+        </p>
+        <h1 className="text-4xl font-black uppercase tracking-[0.18em] sm:text-6xl">
+          Wake Up
+        </h1>
+      </div>
+    </div>
+  );
+}
